9407fcd0901d5e623d10c46b25c5e1f63dfda6fa
Wiktor Klaszczyk <wklaszczyk.npg@gmail.com>
2023-11-25 22:14:05 +0100

    Poprawa

--- Funkcja_celu.py
+++ Funkcja_celu.py
@@ -3,9 +3,14 @@ import math
 
 def time_function(osobnik,S,t_pit) :
     f = 0
-    for n in range(0,len(osobnik)) :
-        f += (Tire.lap_completed()+velocity_e(n)+velocity_m(n))
+    for n in range(0,len(osobnik)) : #osobnik: N*[p,A,o]
+        if osobnik[n][0] == 1:
+            new_tire =  Tire(osobnik[n][2])
+            f += (new_tire.lap_completed(osobnik[n][1])+velocity_e(n)+velocity_m(n))
+        else :
+            f += (new_tire.lap_completed(osobnik[n][1])+velocity_e(n)+velocity_m(n))
 
+    f = f/len(osobnik)
     f = S/f
     f = f+t_pit
     return f
