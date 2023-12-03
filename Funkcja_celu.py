dcbca2153c020c84b3d86983aec559dbd9f63d21
Maria Jagintowicz <mjagintowicz1@gmail.com>
2023-12-03 20:35:23 +0100

    poprawiony arg jak bylo ok to sory

--- Funkcja_celu.py
+++ Funkcja_celu.py
@@ -1,20 +1,20 @@
 from data import *
 import math
 
-def time_function(O,C) :
+def time_function(N, list_of_laps, C): # zmienione arg z O na N, list_of_laps, bo O powstaje dopiero po wyliczeniu tej funkcji
     f = 0
-    for n in range(O.N) : #osobnik: N*[p,A,o]
-        if (n>0 and O.list_of_laps[n][2]!=O.list_of_laps[n-1][2]) or O.list_of_laps[n][0]==1:
-            if O.list_of_laps[n][2]== 1:
+    for n in range(N) : #osobnik: N*[p,A,o]
+        if (n>0 and list_of_laps[n][2]!=list_of_laps[n-1][2]) or list_of_laps[n][0]==1:
+            if list_of_laps[n][2]== 1:
                 pass
-            elif O.list_of_laps[n][2]== 2:
+            elif list_of_laps[n][2]== 2:
                 pass
             else:
                 pass
 
-            f += (new_tire.lap_completed(O.list_of_laps[n][1])+velocity_e(n)+velocity_m(n))
+            f += (new_tire.lap_completed(list_of_laps[n][1])+velocity_e(n)+velocity_m(n))
         else :
-            f += (new_tire.lap_completed(O.list_of_laps[n][1])+velocity_e(n)+velocity_m(n))
+            f += (new_tire.lap_completed(list_of_laps[n][1])+velocity_e(n)+velocity_m(n))
 
     f = f/O.N
     f = C.track_dist/f
