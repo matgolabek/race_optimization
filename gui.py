eb18f4814896c663c8a75d3f49cfde7c9bcb2a5b
Mateusz Gołąbek <100724433+matgolabek@users.noreply.github.com>
2023-12-26 21:12:08 +0100

    wczytywanie i wyświetlanie danych

--- gui.py
+++ gui.py
@@ -1,12 +1,12 @@
 from data import *
 import sys
 from population import *
-from typing import List
+from typing import List, Any
 from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QMessageBox, QFormLayout, \
     QFileDialog, QComboBox, QLineEdit, QGroupBox, QTabWidget, QLabel, QPushButton, QDialog, QDialogButtonBox, \
-    QDoubleSpinBox, QSpinBox
+    QDoubleSpinBox, QSpinBox, QListWidget, QGridLayout
 from PyQt6.QtGui import QFont
-from PyQt6.QtCore import Qt, pyqtSlot
+from PyQt6.QtCore import Qt, pyqtSlot, QRect
 
 
 # OKNO GŁÓWNE
@@ -47,7 +47,6 @@ class MainWindow(QMainWindow):
 
         self.setCentralWidget(tabs)  # umieszczenie zakładek w oknie
 
-
 # ZAKŁADKI
 
 class Config(QWidget):
@@ -62,25 +61,46 @@ class Config(QWidget):
         self.parent = parent
 
         layout_main = QHBoxLayout()  # układ główny
-        box_circuit = QGroupBox('Dane wyścigu')  # układ z danymi wyścigu
-        layout_circuit = QFormLayout()
+        self.box_circuit = QGroupBox('Dane wyścigu')  # układ z danymi wyścigu
+        self.layout_circuit = QFormLayout()
         box_config = QGroupBox('Parametry algorytmu')  # układ z konfiguracją algorytmu
         layout_config = QVBoxLayout()
 
         layout_main.setContentsMargins(20, 20, 20, 20)  # wielkość ramki
         layout_main.setSpacing(20)  # odległości między widżetami
 
-        layout_circuit.addRow('Nazwa:', QLineEdit(self.parent.circuit_name, self))
+        self.layout_circuit.addRow('Nazwa:', QLineEdit(self.parent.circuit_name, self))
         spin_no_laps = QSpinBox(self)
         spin_no_laps.setValue(self.parent.circuit_no_laps)
-        layout_circuit.addRow('Liczba okrążeń:', spin_no_laps)
+        self.layout_circuit.addRow('Liczba okrążeń:', spin_no_laps)
         spin_track_dist = QSpinBox(self)
+        spin_track_dist.setRange(0, 99999)
         spin_track_dist.setValue(self.parent.circuit_track_distance)
-        layout_circuit.addRow('Długość toru [m]:', spin_track_dist)
+        spin_track_dist.setSuffix(" m")
+        self.layout_circuit.addRow('Długość toru:', spin_track_dist)
         spin_t_pit = QDoubleSpinBox(self)
         spin_t_pit.setValue(self.parent.circuit_t_pit)
         spin_t_pit.setDecimals(3)
-        layout_circuit.addRow('Czas pit stopu [s]:', spin_t_pit)
+        spin_t_pit.setSuffix(" s")
+        self.layout_circuit.addRow('Czas pit stopu:', spin_t_pit)
+
+        self.list_tires = QListWidget(self)
+        self.list_tires.setFixedHeight(400)
+        add_button = QPushButton("Dodaj")
+        add_button.clicked.connect(self.add)
+        remove_button = QPushButton("Usuń")
+        remove_button.clicked.connect(self.remove)
+        edit_button = QPushButton("Edytuj")
+        edit_button.clicked.connect(self.edit)
+        clear_button = QPushButton("Wyczyść")
+        clear_button.clicked.connect(self.clear)
+        tires_layout = QGridLayout()
+        tires_layout.addWidget(self.list_tires, 0, 0, 10, 1)
+        tires_layout.addWidget(add_button, 0, 1)
+        tires_layout.addWidget(edit_button, 1, 1)
+        tires_layout.addWidget(remove_button, 2, 1)
+        tires_layout.addWidget(clear_button, 3, 1)
+        self.layout_circuit.addRow('Opony:', tires_layout)
 
         # layout_config - przyciski
         button_read = QPushButton("Wczytaj dane z pliku")   # wczytywanie z pliku
@@ -94,9 +114,9 @@ class Config(QWidget):
         layout_config.addWidget(button_write)
 
         # USTAWIENIA UKŁADU
-        box_circuit.setLayout(layout_circuit)
+        self.box_circuit.setLayout(self.layout_circuit)
         box_config.setLayout(layout_config)
-        layout_main.addWidget(box_circuit)
+        layout_main.addWidget(self.box_circuit)
         layout_main.addWidget(box_config)
         self.setLayout(layout_main)
 
@@ -113,7 +133,217 @@ class Config(QWidget):
         self.parent.circuit_no_laps = circuit.no_laps
         self.parent.circuit_t_pit = circuit.t_pit
         self.parent.circuit_tires = circuit.tires
+        
+        self.layout_circuit.itemAt(0, QFormLayout.ItemRole.FieldRole).widget().setText(self.parent.circuit_name)
+        self.layout_circuit.itemAt(1, QFormLayout.ItemRole.FieldRole).widget().setValue(self.parent.circuit_no_laps)
+        self.layout_circuit.itemAt(2, QFormLayout.ItemRole.FieldRole).widget().setValue(self.parent.circuit_track_distance)
+        self.layout_circuit.itemAt(3, QFormLayout.ItemRole.FieldRole).widget().setValue(self.parent.circuit_t_pit)
+        for tire in circuit.tires:
+            self.layout_circuit.itemAt(4, QFormLayout.ItemRole.FieldRole).layout().itemAt(0).widget().addItem(str(tire))
 
+    @pyqtSlot()
+    def add(self) -> None:
+        params = [0, 0., 0., 0., 0., 0, 0, 0, 0, 0, 0]
+        AddDialog(self, params).exec()
+        tire = Tire(Compound(params[0]), params[5], params[1], params[2], params[3], params[4])
+        for _ in range(params[6]):
+            tire.lap_completed(Aggression.TIRES_MANAGING)
+        for _ in range(params[7]):
+            tire.lap_completed(Aggression.EASY)
+        for _ in range(params[8]):
+            tire.lap_completed(Aggression.NORMAL)
+        for _ in range(params[9]):
+            tire.lap_completed(Aggression.PUSH)
+        for _ in range(params[10]):
+            tire.lap_completed(Aggression.HARD_PUSH)
+        self.parent.circuit_tires.append(tire)
+        self.list_tires.addItem(str(tire))
+
+    @pyqtSlot()
+    def edit(self) -> None:
+        self.list_tires.addItem("text")
+
+    @pyqtSlot()
+    def remove(self) -> None:
+        current_row = self.list_tires.currentRow()
+        if current_row >= 0:
+            current_item = self.list_tires.takeItem(current_row)
+            del current_item
+
+    @pyqtSlot()
+    def clear(self) -> None:
+        self.list_tires.clear()
+
+class AddDialog(QDialog):
+    def __init__(self, parent: QWidget, params: List[Any]) -> None:
+        super().__init__(parent)
+
+        self.params = params
+        self.setWindowTitle("Dodaj oponę")
+        self.layout = QVBoxLayout()
+
+        info_label = QLabel("Uzupełnij parametry opony dane wzorem v(n) = c - k * exp(n * b + d)")
+
+        tire_layout = QFormLayout()
+        compound_combo = QComboBox()
+        compound_combo.addItems(["Miękka", "Pośrednia", "Twarda"])
+        compound_combo.currentIndexChanged.connect(self.update_compund)
+        tire_layout.addRow("Rodzaj mieszanki: ", compound_combo)
+        spin_c = QDoubleSpinBox(self)
+        spin_c.setValue(0.)
+        spin_c.setDecimals(3)
+        spin_c.setRange(-999, 999)
+        spin_c.valueChanged.connect(self.update_c)
+        tire_layout.addRow("Współczynnik c:", spin_c)
+        spin_k = QDoubleSpinBox(self)
+        spin_k.setValue(0.)
+        spin_k.setDecimals(3)
+        spin_k.setRange(-999, 999)
+        spin_k.valueChanged.connect(self.update_k)
+        tire_layout.addRow("Współczynnik k:", spin_k)
+        spin_b = QDoubleSpinBox(self)
+        spin_b.setValue(0.)
+        spin_b.setDecimals(3)
+        spin_b.setRange(-999, 999)
+        spin_b.valueChanged.connect(self.update_b)
+        tire_layout.addRow("Współczynnik b:", spin_b)
+        spin_d = QDoubleSpinBox(self)
+        spin_d.setValue(0.)
+        spin_d.setDecimals(3)
+        spin_d.setRange(-999, 999)
+        spin_d.valueChanged.connect(self.update_d)
+        tire_layout.addRow("Współczynnik d:", spin_d)
+        spin_max_laps = QSpinBox(self)
+        spin_max_laps.setValue(0)
+        spin_max_laps.valueChanged.connect(self.update_max_laps)
+        tire_layout.addRow("Maksymalna liczba okrążeń:", spin_max_laps)
+        spin_a0 = QSpinBox(self)
+        spin_a0.setValue(0)
+        spin_a0.valueChanged.connect(self.update_a0)
+        tire_layout.addRow("Przejechane okrążenia z agresywnością 0:", spin_a0)
+        spin_a1 = QSpinBox(self)
+        spin_a1.setValue(0)
+        spin_a1.valueChanged.connect(self.update_a1)
+        tire_layout.addRow("Przejechane okrążenia z agresywnością 1:", spin_a1)
+        spin_a2 = QSpinBox(self)
+        spin_a2.setValue(0)
+        spin_a2.valueChanged.connect(self.update_a2)
+        tire_layout.addRow("Przejechane okrążenia z agresywnością 2:", spin_a2)
+        spin_a3 = QSpinBox(self)
+        spin_a3.setValue(0)
+        spin_a3.valueChanged.connect(self.update_a3)
+        tire_layout.addRow("Przejechane okrążenia z agresywnością 3:", spin_a3)
+        spin_a4 = QSpinBox(self)
+        spin_a4.setValue(0)
+        spin_a4.valueChanged.connect(self.update_a4)
+        tire_layout.addRow("Przejechane okrążenia z agresywnością 4:", spin_a4)
+
+        add_button = QPushButton("Dodaj")
+        add_button.clicked.connect(self.accept)
+
+        self.layout.addWidget(info_label)
+        self.layout.addLayout(tire_layout)
+        self.layout.addWidget(add_button)
+        self.setLayout(self.layout)
+
+    @pyqtSlot(int)
+    def update_compund(self, compound: int) -> None:
+        """
+        Uaktualnienie mieszanki
+        :param compund: (int) : mieszanka z ComboBox
+        :return: None
+        """
+        self.params[0] = compound
+
+    @pyqtSlot(float)
+    def update_c(self, c: float) -> None:
+        """
+        Uaktualnienie współczynnika c
+        :param c: (float) : współczynnik
+        :return: None
+        """
+        self.params[1] = c
+
+    @pyqtSlot(float)
+    def update_k(self, k: float) -> None:
+        """
+        Uaktualnienie współczynnika c
+        :param k: (float) : współczynnik
+        :return: None
+        """
+        self.params[2] = k
+
+    @pyqtSlot(float)
+    def update_b(self, b: float) -> None:
+        """
+        Uaktualnienie współczynnika b
+        :param b: (float) : współczynnik
+        :return: None
+        """
+        self.params[3] = b
+
+    @pyqtSlot(float)
+    def update_d(self, d: float) -> None:
+        """
+        Uaktualnienie współczynnika d
+        :param d: (float) : współczynnik
+        :return: None
+        """
+        self.params[4] = d
+
+    @pyqtSlot(int)
+    def update_max_laps(self, max_laps: int) -> None:
+        """
+        Uaktualnienie maksymalnej liczby opkrążeń
+        :param max_laps: (int) : liczba okrążeń
+        :return: None
+        """
+        self.params[5] = max_laps
+
+    @pyqtSlot(int)
+    def update_a0(self, a0: int) -> None:
+        """
+        Uaktualnienie liczby okrążeń przejechanej z z agresywnością 0
+        :param a0: (int) : liczba okrążeń
+        :return: None
+        """
+        self.params[6] = a0
+
+    @pyqtSlot(int)
+    def update_a1(self, a1: int) -> None:
+        """
+        Uaktualnienie liczby okrążeń przejechanej z z agresywnością 1
+        :param a1: (int) : liczba okrążeń
+        :return: None
+        """
+        self.params[7] = a1
+
+    @pyqtSlot(int)
+    def update_a2(self, a2: int) -> None:
+        """
+        Uaktualnienie liczby okrążeń przejechanej z z agresywnością 2
+        :param a2: (int) : liczba okrążeń
+        :return: None
+        """
+        self.params[8] = a2
+
+    @pyqtSlot(int)
+    def update_a3(self, a3: int) -> None:
+        """
+        Uaktualnienie liczby okrążeń przejechanej z z agresywnością 3
+        :param a3: (int) : liczba okrążeń
+        :return: None
+        """
+        self.params[9] = a3
+
+    @pyqtSlot(int)
+    def update_a4(self, a4: int) -> None:
+        """
+        Uaktualnienie liczby okrążeń przejechanej z z agresywnością 4
+        :param a4: (int) : liczba okrążeń
+        :return: None
+        """
+        self.params[10] = a4
 
 class Solution(QWidget):
 
